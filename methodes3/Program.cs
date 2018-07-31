2e6ad7ebc382d669e780a316b6441722fe0cfdbf
AlexinaO <alexina.orsoni@gmail.com>
2018-07-31 17:30:12 +0200

    Exercices changement type

---
+++ methodes3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodes3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var saisie = SaisirEntier("Saisissez un entier:");
+            Console.WriteLine(saisie);
+            Console.ReadKey();
+
+
+        }
+        static int SaisirEntier(string texteAAfficher)
+        {
+            Console.WriteLine(texteAAfficher);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
